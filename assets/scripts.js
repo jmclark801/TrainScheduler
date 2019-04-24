ae47e8b323351bb362985e597328234b4f23a9e9
NunyaBizness <jmclark801@yahoo.com>
2019-04-24 16:38:27 -0600

    Update assets and styling

--- assets/scripts.js
+++ assets/scripts.js
@@ -18,52 +18,49 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $("#button-submit").on('click', function(){
-  trainName = $('#train-name').val().trim()
-  destination = $('#destination').val().trim()
-  firstTrainTime = $('#first-train-time').val().trim()
-  frequency = $('#frequency').val().trim()
-  console.log(trainName)
-
-  database.ref().push({
-    trainName: trainName,
-    destination: destination,
-    firstTrainTime: firstTrainTime,
-    frequency: frequency,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  })
+  if ($('#train-name').val() !== "" && $('#destination').val() !== "" && $('#first-train-time').val() !== "" && $('#frequency').val() !== ""){
+    trainName = $('#train-name').val().trim()
+    destination = $('#destination').val().trim()
+    firstTrainTime = $('#first-train-time').val().trim()
+    frequency = $('#frequency').val().trim()
+    console.log(trainName)
+    $('#train-name').val("")
+    $('#tdestination').val("")
+    $('#first-train-time').val("")
+    $('#frequency').val("")
+  
+    database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
+  } 
 })
 
 database.ref().on('child_added', function(snapshot){
-  var data = snapshot.val()
+  var data = snapshot.val() 
   var firstTrainTime = data.firstTrainTime
-  var firstTrainTimeMoment = moment(firstTrainTime, "HH:mm")
-  var minutesAway = 12 
-  var nextArrival = 12;
-  var nowMoment = moment()
-  var firstTrainTimeMoment = moment(data.firstTrainTime, "HH:mm")
-  var timeFromFirstTrain = nowMoment.diff(firstTrainTimeMoment)
-  var duration = moment.duration(nowMoment.diff(firstTrainTimeMoment))
+  var frequency = data.frequency
+  
+  var firstTrainTimeMoment = moment(firstTrainTime, "HH:mm").subtract(1, "day")
+  console.log("First Train Time moment", firstTrainTimeMoment)
+ 
+  var currentTimeMoment = moment()
+  console.log("Current Time Moment: ", currentTimeMoment.format("HH:mm"))
 
-  console.log("Now moment: ", nowMoment.format("HH:mm"))
-  console.log("First Train Time", firstTrainTime)
-  console.log("First Train Time moment", firstTrainTimeMoment.format("HH:mm"))
-  console.log("Time from first Train: ", moment(timeFromFirstTrain).format("HH:mm"))
-  console.log("reversed", moment(firstTrainTimeMoment.diff(nowMoment)).format("HH:mm"))
-  console.log("stack overflow solution", moment(duration).format("HH:mm"))
-  
-  // current time in minutes from FirstTrainTime(3AM = 180 minutes) - firstTrainMinutes using DIFF from EPOCH OK.
-  // In example, this is 120 minutes.
-  // difference % frequency = 0 = 0 minutes away.
-  // time + difference % frequncy = Next Arrival time (converted to time)
-  
-  
-  $("#table-data").append("<tr><td>" + data.trainName + "</td><td>" + data.destination + "</td><td>" + data.frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>")
+  var diffTime = moment().diff(moment(firstTrainTimeMoment), "minutes")
+  console.log("Difference in time: ", diffTime)
+
+  var remainder = diffTime % frequency
+  console.log("Remainder: ", remainder)
+
+  var timeTilNextTrain = frequency - remainder
+  console.log("minutes till train", timeTilNextTrain)
+
+  var nextTrainDepartureTime = moment().add(timeTilNextTrain, "minutes")
+  console.log("Next Train Departs at: ", nextTrainDepartureTime.format("hh:mm"))
+     
+  $("#table-data").append("<tr><td>" + data.trainName + "</td><td>" + data.destination + "</td><td>" + frequency + "</td><td>" + nextTrainDepartureTime.format("hh:mm") + "</td><td>" + timeTilNextTrain + "</td></tr>")
 })
-
-
-
-
-// On submit of the form, get the form details and save them to firebase using .push
-// create event listener on change of database .ref()
-  // Update table with all entries
-  // Need formatting using Moment.js to calculate minutes away, arrival time, etc.
